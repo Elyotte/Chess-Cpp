902a6d71d72d7219f1018bcfcc553471b3e1d575
Eliott <tan.eliott04@gmail.com>
2025-11-17 10:26:22 +0100

    installing raylib library

--- Chess/Source.cpp
+++ Chess/Source.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "raylib\include\raylib.h"
 
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
+    InitWindow(800, 600, "Raylib !");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello!", 10, 10, 20, BLACK);
+        EndDrawing();
+    }
 }
